7cbb0520d32fb89e0c6f9aacd0edffb404854d00
Shakil Ahmed <shakilcse25@gmail.com>
2025-04-21 13:24:44 +0600

    fix: server deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+// Home.tsx or page.tsx
 import SearchBar from "./components/SearchBar";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 export default function Home() {
   return (
@@ -14,7 +17,9 @@ export default function Home() {
         <SearchBar />
       </div>
 
-      <BookList />
+      <Suspense fallback={<LoadingSpinner />}>
+        <BookList />
+      </Suspense>
     </div>
   );
 }
